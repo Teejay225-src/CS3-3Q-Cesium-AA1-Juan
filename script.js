6e7cba5dfe27550da75ce338385702273c78e695
Teejay225-src <tiejuan22@src.pshs.edu.ph>
2025-01-23 18:29:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+let r =
+let c = 2*PI*r;
+let surfArea = 4*PI*r*r;
+let vol = (4/3)*(PI*r*r*r);
+
+document.write('The radius of the sphere is' + r + '<br><br'>);
+document.write('The circumference is' + c + '<br><br'>);
+document.write('The surface area is' + surfArea + '<br><br'>);
+document.write('The volume is' + vol + '<br><br'>);
